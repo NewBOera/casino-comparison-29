fcc5cdad3d4d0f65139dfcf3b1e70009203cb820
Álvaro Torres <alvaro.espindola.torres@gmail.com>
2025-01-27 08:50:11 -0600

    chore: deploy branch

--- src/components/ImgAh.js
+++
@@ -1,9 +0,0 @@
-import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
-
-export class ImgAh extends LitElement {
-  render() {
-    return html``;
-  }
-}
-
-customElements.define('img-after-hero', ImgAh);
